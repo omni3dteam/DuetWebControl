ba693fcea58f9a7e975d78402d19158f28e2fed7
MSzOmni3D <msz@omni3d.net>
2022-11-02 13:56:56 +0100

    Change Status screen panels size on LCD

--- src/components/panels/FFFContainerPanel.vue
+++ src/components/panels/FFFContainerPanel.vue
@@ -1,10 +1,10 @@
 <template>
 	<v-row :dense="$vuetify.breakpoint.mobile">
-		<v-col cols="12" sm="6" md="4" lg="4" xl="4">
+		<v-col cols="12" sm="12" md="4" lg="4" xl="4">
 			<status-panel></status-panel>
 		</v-col>
 
-		<v-col cols="12" sm="6" md="5" lg="5" xl="4">
+		<v-col cols="12" sm="12" md="5" lg="5" xl="4">
 			<tools-panel></tools-panel>
 		</v-col>
 
