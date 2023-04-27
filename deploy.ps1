302552bebef4061d841c454d899c8febab072b75
hubertbiedka <hb@omni3d.net>
2023-04-27 08:23:18 +0200

    Simple auto-deploy script added

---
+++ deploy.ps1
@@ -0,0 +1,12 @@
+#build DWC, copy into 192.168.1.{ip} and run deploy-dwc script onboard
+
+$ip=$args[0]
+
+npm run build
+scp .\dist\DuetWebControl-SBC.zip pi@192.168.1.${ip}:/tmp
+ssh pi@192.168.1.${ip} "
+    rm -r /tmp/dwc &&
+    mkdir /tmp/dwc &&
+    unzip /tmp/DuetWebControl-SBC.zip -d /tmp/dwc &&
+    sudo -u dsf cp -rv /tmp/dwc/* /opt/dsf/dwc/
+"
